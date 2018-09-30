09a604b0cd17ece5735a683ed70dee26b1b47db7
Olivier Despesse <olived19@gmail.com>
2018-09-30 20:53:37 +0200

    Install a newer version of Ansible on Debian 8

--- debian8/install-ansible.sh
+++ debian8/install-ansible.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 apt-get update
-apt-get -y install ansible python-apt aptitude
+apt-get -y install python-pip python-apt aptitude python-paramiko python-jinja2 python-yaml
+pip install ansible
+apt-get -y purge python-pip
